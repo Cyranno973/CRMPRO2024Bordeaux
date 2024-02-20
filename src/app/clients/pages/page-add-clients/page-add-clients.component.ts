4e001337fd934b730cc5fd63dcff4e1075ac6174
cyran <gs.gouin@gmail.com>
2024-02-20 17:13:52 +0100

    validateur formClient

--- src/app/clients/pages/page-add-clients/page-add-clients.component.ts
+++ src/app/clients/pages/page-add-clients/page-add-clients.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-page-add-clients',
@@ -13,10 +13,15 @@ export class PageAddClientsComponent implements OnInit {
   }
   ngOnInit() {
     this.formClient = this.fb.group({
-      name:['aeaea'],
-      firstName:[''],
+      name:['',[Validators.required, Validators.maxLength(5)]],
+      firstName:['', [Validators.required, Validators.minLength(2)]],
       phone:[''],
-      email:[''],
+      email:['', ],
     })
+  }
+
+  save() {
+    console.log(this.formClient);
+    console.log(this.formClient.value);
   }
 }
